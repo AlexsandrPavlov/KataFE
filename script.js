774e312021772968069795708b503383a624e822
Alexsandr Pavlov <buterfeed@gmail.com>
2024-11-29 16:16:38 +0300

    блюр с модалками 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const btn_call_close = document.querySelector('.modal-call__btn-close');
 const btn_message_close = document.querySelector('.modal-message__btn-close');
 const btn_call_dd = document.querySelector( '.dropdown-footer-button__call');
 const btn_message_dd = document.querySelector( '.dropdown-footer-button__chat');
+const body = document.querySelector('body')
+const veil = document.querySelector('.page-container__veil')
+
 // блоки
 const dropdown =  document.querySelector('.dropdown-container');
 const modal_call = document.querySelector('.modal-call');
@@ -14,25 +17,42 @@ const modal_message = document.querySelector('.modal-message')
 
 btn_burger.addEventListener('click', function(){
     dropdown.classList.add('dropdown-container__active')
+    veil.classList.add('page-container__veil--active')
 })
 btn_burger_close.addEventListener('click', function(){
     dropdown.classList.remove('dropdown-container__active')
+    veil.classList.remove('page-container__veil--active')
 })
 btn_call.addEventListener('click', function(){
     modal_call.classList.add('modal-call__active')
+    veil.classList.add('page-container__veil--active')
 })
 btn_call_dd.addEventListener('click', function(){
     modal_call.classList.add('modal-call__active')
+    veil.classList.add('page-container__veil--active')
+
 })
 btn_call_close.addEventListener('click', function(){
     modal_call.classList.remove('modal-call__active')
+    veil.classList.remove('page-container__veil--active')
+
 })
 btn_message.addEventListener('click', function(){
     modal_message.classList.add('modal-message__active')
+    veil.classList.add('page-container__veil--active')
 })
 btn_message_dd.addEventListener('click', function(){
     modal_message.classList.add('modal-message__active')
+    veil.classList.add('page-container__veil--active')
+
 })
 btn_message_close.addEventListener('click', function(){
     modal_message.classList.remove('modal-message__active')
+    veil.classList.remove('page-container__veil--active')
 })
+ veil.addEventListener('click' , function(){
+    dropdown.classList.remove('dropdown-container__active')
+    modal_call.classList.remove('modal-call__active')
+    modal_message.classList.remove('modal-message__active')
+    veil.classList.remove('page-container__veil--active')
+ })
